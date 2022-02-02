0ecd403ce85c30a08cfb92cfbcd3880681ca20c3
AndreyV <95086532+AndreyV0977@users.noreply.github.com>
2022-02-02 23:20:46 +0300

    Add files via upload

---
+++ APPtest.xcodeproj/project.xcworkspace/xcshareddata/APPtest/ThreeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ThreeTableViewCell.swift
+//  APPtest
+//
+//  Created by ЛИС on 30.01.2022.
+//
+
+import UIKit
+
+class ThreeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
